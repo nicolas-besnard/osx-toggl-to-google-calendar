e4118dafb271d598eee75baba0df58072f4b6b5c
Nicolas Besnard <besnard.nicolas@gmail.com>
2015-02-20 22:42:24 +0000

    Functionnal menu

--- TogglGoogleCalendar/AppDelegate.swift
+++ TogglGoogleCalendar/AppDelegate.swift
@@ -9,19 +9,46 @@
 import Cocoa
 
 @NSApplicationMain
+
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     @IBOutlet weak var window: NSWindow!
-
-
+    var statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(CGFloat(-1))
+    
+    var menu = NSMenu()
+    
+    
+    @IBOutlet weak var statusMenu: NSMenu!
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        statusItem.title = "Title"
+        
+        statusItem.image = NSImage(named: "toggl-icon")
+        statusItem.alternateImage = NSImage(named: "toggl-icon")
+        statusItem.highlightMode = true
+        statusItem.menu = menu
+        
+        //Add menuItem to menu
+        var menuItem : NSMenuItem = NSMenuItem()
+        menuItem.title = "Configuration"
+        menuItem.action = Selector("clickConfigurationItem")
+        menuItem.keyEquivalent = ""
+        
+        
+        menu.addItem(menuItem)
+        
+        self.window!.orderOut(self)
     }
-
+    
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func clickConfigurationItem() {
+        NSApp.activateIgnoringOtherApps(true)
+        self.window.orderFront(self)
+    }
+    
 }
+
 
