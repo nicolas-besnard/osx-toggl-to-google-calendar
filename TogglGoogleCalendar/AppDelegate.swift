e667738b77f11f149b63692acc7c8abcaf10427d
Nicolas Besnard <besnard.nicolas@gmail.com>
2015-02-20 20:52:34 +0000

    init

---
+++ TogglGoogleCalendar/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TogglGoogleCalendar
+//
+//  Created by Nicolas Besnard on 20/02/2015.
+//  Copyright (c) 2015 Nicolas Besnard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TogglGoogleCalendarTests/TogglGoogleCalendarTests.swift
@@ -0,0 +1,36 @@
+//
+//  TogglGoogleCalendarTests.swift
+//  TogglGoogleCalendarTests
+//
+//  Created by Nicolas Besnard on 20/02/2015.
+//  Copyright (c) 2015 Nicolas Besnard. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class TogglGoogleCalendarTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
