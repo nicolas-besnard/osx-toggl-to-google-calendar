76a747fcff3eb7ee22b23349e4e68f717d6b1774
Nicolas Besnard <besnard.nicolas@gmail.com>
2015-02-21 17:54:13 +0000

    Working Request

--- TogglGoogleCalendar/AppDelegate.swift
+++ TogglGoogleCalendar/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import Cocoa
 
+
 @NSApplicationMain
 
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -38,6 +39,42 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         menu.addItem(menuItem)
         
         self.window!.orderOut(self)
+        let manager = AFHTTPRequestOperationManager()
+        manager.responseSerializer = AFJSONResponseSerializer()
+        manager.requestSerializer = AFHTTPRequestSerializer()
+        manager.requestSerializer.setAuthorizationHeaderFieldWithUsername("", password: "api_token")
+
+        manager.GET("https://www.toggl.com/api/v8/me", parameters: nil, success: { (operation, responseObject) -> Void in
+            println(operation)
+            println(responseObject)
+        }) { (operation, error) -> Void in
+            println("-- ERROR --")
+            println(operation)
+            println(error)
+        }
+        
+//        [manager GET:@"http://example.com/resources.json" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+//            NSLog(@"JSON: %@", responseObject);
+//        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//            NSLog(@"Error: %@", error);
+//        }];
+        
+//        AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+//        
+//        NSURLCredential *credential = [NSURLCredential credentialWithUser:@"user" password:@"passwd" persistence:NSURLCredentialPersistenceNone];
+//        
+//        NSMutableURLRequest *request = [manager.requestSerializer requestWithMethod:@"GET" URLString:@"https://httpbin.org/basic-auth/user/passwd" parameters:nil];
+//        
+//        AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+//        [operation setCredential:credential];
+//        [operation setResponseSerializer:[AFJSONResponseSerializer alloc]];
+//        [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+//        NSLog(@"Success: %@", responseObject);
+//        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+//        NSLog(@"Failure: %@", error);
+//        }];
+//        [manager.operationQueue addOperation:operation];
+
     }
     
     func applicationWillTerminate(aNotification: NSNotification) {
