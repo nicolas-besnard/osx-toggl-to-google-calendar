c01e89dee026bc61039268210e3d62ba51fb8aa4
Nicolas Besnard <besnard.nicolas@gmail.com>
2015-02-26 15:14:42 +0000

    functionnal login

--- app/TogglGoogleCalendar/LoginViewController.swift
+++ app/TogglGoogleCalendar/LoginViewController.swift
@@ -10,8 +10,30 @@ import Cocoa
 
 class LoginViewController: NSViewController {
 
+    @IBOutlet weak var emailTextField: NSTextField!
+    @IBOutlet weak var passwordTextField: NSSecureTextField!
+    @IBOutlet weak var loginButton: NSButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+    }
+    
+    @IBAction func clickLoginButton(sender: AnyObject) {
+        let email = emailTextField.stringValue
+        let password = passwordTextField.stringValue
+        
+        if email.length == 0 || !email.isEmail() {
+            emailTextField.becomeFirstResponder()
+        }
+        else if password.length == 0{
+            passwordTextField.becomeFirstResponder()
+        } else {
+            loginButton.enabled = false
+            
+            Context.shared.userService.loginWithEmail(email, password: password) { () -> Void in
+                self.loginButton.enabled = true
+            }
+        }
     }
 }
