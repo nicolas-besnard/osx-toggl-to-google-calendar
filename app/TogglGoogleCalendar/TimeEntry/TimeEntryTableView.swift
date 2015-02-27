e30474cbdd0f81308aaa494fe62836e6f55ce35e
Nicolas Besnard <besnard.nicolas@gmail.com>
2015-02-27 16:43:04 +0000

    Clean repo

---
+++ app/TogglGoogleCalendar/TimeEntry/TimeEntryTableView.swift
@@ -0,0 +1,19 @@
+//
+//  TimeEntryTableView.swift
+//  TogglGoogleCalendar
+//
+//  Created by Nicolas Besnard on 27/02/2015.
+//  Copyright (c) 2015 Nicolas Besnard. All rights reserved.
+//
+
+import Cocoa
+
+class TimeEntryTableView: NSTableView {
+
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
